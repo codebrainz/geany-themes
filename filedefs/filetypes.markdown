12721944df6e2874f832cb90c5073cea8b6af6ca
Matthew Brush <codebrainz@codebrainz.ca>
2011-02-23 16:06:00 -0800

    Initial commit

---
+++ filedefs/filetypes.markdown
@@ -0,0 +1,29 @@
+# For complete documentation of this file, please see Geany's main documentation
+[styling]
+# foreground;background;bold;italic
+default=default
+strong=tag
+emphasis=tag
+header1=keyword_1,bold
+header2=keyword_1,bold
+header3=keyword_1,bold
+header4=keyword_1,bold
+header5=keyword_1,bold
+header6=keyword_1,bold
+ulist_item=keyword_2,bold
+olist_item=keyword_2,bold
+blockquote=tag
+strikeout=tag
+hrule=tag
+link=type
+code=attribute
+codebk=attribute
+
+[settings]
+# default extension used when saving files
+extension=mdml
+
+# sort tags by appearance
+symbol_list_sort_mode=1
+
+
---
+++ filedefs/filetypes.sh
@@ -0,0 +1,49 @@
+# For complete documentation of this file, please see Geany's main documentation
+[styling]
+# Edit these in the colorscheme .conf file intead
+default=default
+commentline=comment
+number=number_1
+word=keyword_1
+string=string_1
+character=string_1
+operator=operator
+identifier=identifier_1
+backticks=backtick
+param=parameter
+scalar=number
+error=error
+here_delim=operator
+here_q=here_doc
+
+[keywords]
+primary=break case continue do done elif else esac eval exit export fi for function goto if in integer return set shift then until while
+
+
+[settings]
+# default extension used when saving files
+extension=sh
+
+# the following characters are these which a "word" can contains, see documentation
+#wordchars=_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789
+
+# if only single comment char is supported like # in this file, leave comment_close blank
+comment_open=#
+comment_close=
+
+# set to false if a comment character/string should start a column 0 of a line, true uses any
+# indentation of the line, e.g. setting to true causes the following on pressing CTRL+d
+	#command_example();
+# setting to false would generate this
+#	command_example();
+# This setting works only for single line comments
+comment_use_indent=true
+
+# context action command (please see Geany's main documentation for details)
+context_action_cmd=
+
+[build_settings]
+# %f will be replaced by the complete filename
+# %e will be replaced by the filename without extension
+# (use only one of it at one time)
+run_cmd="./%f"
